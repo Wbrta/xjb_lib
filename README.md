5e9e45ce8cba260e24b1b7c23ea0a1280f0a0f3e
Wbrta <ghlovezxcvbnm@vip.qq.com>
2017-08-18 17:11:12 +0800

    add some function of skip_list.h && remove skip_list.cpp && add rb_tree.h

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wiklvrain_lib
+
+## Introduction
+This repository is my own library.
+Here will store some code which I write when I learn or work.
+And maybe this code can be use in some environment.
+And this repository is only something i play.
--- skip_list.h
+++ skip_list.h
@@ -1,32 +1,93 @@
 #ifndef SKIP_LIST_H
 #define SKIP_LIST_H
 
-#include <initializer_list>
+#include <stddef.h>
+#include <stdlib.h>
 
-template<typename T>
-typedef struct skip_list_structure {
-public:
-	skip_list_structure(T _value) : value(_value) {
-		level = 0;
-		forward = nullptr;
-	}
-private:
-	using SKIP_LIST_POINTER = skip_list_structure<T>*;
-
-	T value;
-	int level;
-	SKIP_LIST_POINTER forward;
+template<typename K, typename V = int>
+struct skip_list_node {
+  K key;
+  V value;
+  skip_list_node* forward[1];
 };
 
-template<typename T>
+template<typename K, typename V = int>
 class skip_list {
+protected:
+  typedef K key_type;
+  typedef V value_type;
+  typedef skip_list_node<K, V> Node;
+  typedef Node* pNode;
 public:
-	skip_list();
-	skip_list(initializer_list<T>&);
-	virtual ~skip_list();
+  skip_list(int max_level);
+  ~skip_list();
+  
+  bool insert(key_type key);
+  bool insert(key_type key, value_type value);
+  
+  bool erase(key_type key);
+  bool erase_once(key_type key);
+  
+  bool exist(key_type key);
+
+  pNode find(key_type key);
 private:
-	using skip_list_pointer = skip_list_structure<T>*;
-	skip_list_pointer skip_list_header;
+  pNode start;
+  pNode finish;
+
+  const size_t MAX_LEVEL;
+  
+  void random_level();
 };
+
+template<typename K, typename V>
+skip_list<K, V>::skip_list(int max_level) : MAX_LEVEL(max_level) {
+  start = (pNode)malloc(sizeof(Node) + MAX_LEVEL * sizeof(pNode));
+  finish = (pNode)malloc(sizeof(Node) + MAX_LEVEL * sizeof(pNode));
+}
+
+template<typename K, typename V>
+skip_list<K, V>::~skip_list() {
+  free(start);
+  free(finish);
+}
+
+template<typename K, typename V>
+bool skip_list<K, V>::insert(key_type key) {
+  return insert(key, value_type(0));
+}
+
+template<typename K, typename V>
+bool skip_list<K, V>::insert(key_type key, value_type value) {
+  
+}
+
+template<typename K, typename V>
+bool skip_list<K, V>::erase(key_type key) {
+
+}
+
+template<typename K, typename V>
+bool skip_list<K, V>::erase_once(key_type key) {
+
+}
+
+template<typename K, typename V>
+bool skip_list<K, V>::exist(key_type key) {
+
+}
+
+template<typename K, typename V>
+typename skip_list<K, V>::pNode skip_list<K, V>::find(key_type key) {
+
+}
+
+template<typename K, typename V>
+void skip_list<K, V>::random_level() {
+  int level = 1;
+  while (rand() % 2) ++level;
+  level = (level <= MAX_LEVEL ? level : MAX_LEVEL);
+  return level;
+}
 
 #endif
