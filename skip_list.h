e064296cebaec895ada830ec6501e678d673659a
Wbrta <ghlovezxcvbnm@vip.qq.com>
2017-08-21 11:15:33 +0800

    add some code of skip_list.h about insert/new_node and repair some bugs

--- skip_list.h
+++ skip_list.h
@@ -40,17 +40,28 @@ private:
 
   const size_t MAX_LEVEL;
   
-  void random_level();
+  int random_level();
+  pNode new_node(K key, V value);
 };
 
 template<typename K, typename V>
 skip_list<K, V>::skip_list(int max_level) : MAX_LEVEL(max_level) {
   start = (pNode)malloc(sizeof(Node) + MAX_LEVEL * sizeof(pNode));
   finish = (pNode)malloc(sizeof(Node) + MAX_LEVEL * sizeof(pNode));
+  for (int i = 0; i < MAX_LEVEL; ++i)
+    start->forward[i] = NPOS;
+  for (int i = 0; i < MAX_LEVEL; ++i)
+    finish->forward[i] = NPOS;
 }
 
 template<typename K, typename V>
 skip_list<K, V>::~skip_list() {
+  pNode node = start->forward[0];
+  while (node != finish) {
+    pNode &tmp = node->forward[0];
+    free(node);
+    node = tmp;
+  }
   free(start);
   free(finish);
 }
@@ -62,19 +73,39 @@ bool skip_list<K, V>::insert(key_type key) {
 
 template<typename K, typename V>
 bool skip_list<K, V>::insert(key_type key, value_type value) {
-   
+  pNode* update = new pNode[MAX_LEVEL];
+  for (int i = 0; i < MAX_LEVEL; ++i) {
+    pNode node = start;
+    while (node->forward[i] != finish && node->forward[i]->key < key) 
+      node = node->forward[i];
+    update[i] = &node;
+  }
+  
+  pNode node = new_node(key, value);
+  if (node == NPOS) {
+    delete[] update;
+    return false;
+  }
+
+  for (int i = 0; i < MAX_LEVEL; ++i) {
+    node->forward[i] = (*update[i])->forward[i];
+    (*update[i])->forward[i] = node;
+  }
+
+  delete[] update;
+  return true;
 }
 
 template<typename K, typename V>
 bool skip_list<K, V>::erase(key_type key) {
   bool is_success = erase_once(key);
-  while (erase_once(key));
+  while (is_success && erase_once(key));
   return is_success;
 }
 
 template<typename K, typename V>
 bool skip_list<K, V>::erase_once(key_type key) {
-
+  
 }
 
 template<typename K, typename V>
@@ -106,11 +137,21 @@ typename skip_list<K, V>::pNode skip_list<K, V>::find(key_type key) {
 }
 
 template<typename K, typename V>
-void skip_list<K, V>::random_level() {
+int skip_list<K, V>::random_level() {
   int level = 1;
   while (rand() % 2) ++level;
   level = (level <= MAX_LEVEL ? level : MAX_LEVEL);
   return level;
+}
+
+template<typename K, typename V>
+typename skip_list<K, V>::pNode skip_list<K, V>::new_node(K key, V value) {
+  int level = random_level();
+  pNode node = (pNode)malloc(sizeof(Node) + level * sizeof(pNode));
+  node->key = key;
+  node->value = value;
+  node->level = level;
+  return node;
 }
 
 #endif
