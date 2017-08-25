6eeece63d09a948319e74bfabada884541515f0f
Wbrta <ghlovezxcvbnm@vip.qq.com>
2017-08-25 11:41:18 +0800

    initialize rb_tree.h

--- rb_tree.h
+++ rb_tree.h
@@ -3,14 +3,37 @@
 
 namespace wiklvrain {
 
+const bool RED = false;
+const bool BLACK = true;
+
+template<typename K, typename V = int>
+struct rb_tree_node {
+  typedef rb_tree_node<K, V>* pRBnode;
+  
+  K key;
+  V value;
+  bool color;
+  pRBnode left, right;
+  
+  rb_tree_node(K _key, V _value, bool _color = BLACK): key(_key), value(_value), color(_color) {
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
 template<typename K, typename V = int>
 class rb_tree {
 public:
 protected;
   typedef K key_type;
   typedef V value_type;
-  typedef rb_tree<K, V>* pNode;
+  typedef rb_tree_node<K, V> Node;
+  typedef Node* pNode;
 private:
+  void left_rotate();
+  void right_rotate();
+
+  Node root;
 };
 }
 
