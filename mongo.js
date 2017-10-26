00cf61494316161ab374feaf573afb52ade6cb8b
Wbrta <ghlovezxcvbnm@vip.qq.com>
2017-10-26 12:03:06 +0800

    repair tab and space...mmp

--- mongo.js
+++ mongo.js
@@ -17,10 +17,10 @@ first_values.push.apply(first_values, tmp);
 var trailing_values = byte_range(0x80, 0xBF);
 
 var get_rand_int = function(min, base) {
-	return min + Math.ceil(Math.random() * base);
+  return min + Math.ceil(Math.random() * base);
 }
 var get_rand_float = function(base) {
-	return Math.random() * base;
+  return Math.random() * base;
 }
 var random_choice = function(array) {
   var index = Math.floor((Math.random() * array.length));
@@ -73,90 +73,90 @@ var get_rand_string = function(len) {
   return string;
 }
 var get_rand_object = function() {
-	var ret = {};
-	var len = get_rand_int(5, 10);
-	for (var i = 0; i < len; ++i) 
-		ret[get_rand_string(get_rand_int(8, 120))] = get_rand_string(get_rand_int(16, 240));
-	return ret;
+  var ret = {};
+  var len = get_rand_int(5, 10);
+  for (var i = 0; i < len; ++i) 
+    ret[get_rand_string(get_rand_int(8, 120))] = get_rand_string(get_rand_int(16, 240));
+  return ret;
 }
 var get_date = function() {
-	var time = new Date().toLocaleString();
-	return time;
+  var time = new Date().toLocaleString();
+  return time;
 }
 var Insert = function(db, isIndex) {
-	var len = get_rand_int(1, 5);
-	for (var i = 0; i < len; ++i) {
-		key[i] = get_rand_string(get_rand_int(2, 30));
-		value_type[i] = get_rand_int((i == 0 ? 1 : 0), 5);
+  var len = get_rand_int(1, 5);
+  for (var i = 0; i < len; ++i) {
+    key[i] = get_rand_string(get_rand_int(2, 30));
+    value_type[i] = get_rand_int((i == 0 ? 1 : 0), 5);
     console.log(key[i]);
-	}
-	var datas = [];
-	len = get_rand_int(1, 5);
-	for (var cur = 0; cur < len; ++cur) {
-		var data = {};
-		for (var i = 0; i < key.length; ++i) {
-			var rand = value_type[i];
-			if (rand == 0) data[key[i]] = get_rand_object();
-			else if (rand == 1) data[key[i]] = get_rand_int(0, 1e9 + 7);
-			else if (rand == 2) data[key[i]] = get_rand_float(1e9 + 7);
-			else if (rand == 3) data[key[i]] = get_rand_string(get_rand_int(16, 240));
-			else data[key[i]] = get_date();
-		}
-		datas[cur] = data;
-	}
+  }
+  var datas = [];
+  len = get_rand_int(1, 5);
+  for (var cur = 0; cur < len; ++cur) {
+    var data = {};
+    for (var i = 0; i < key.length; ++i) {
+      var rand = value_type[i];
+      if (rand == 0) data[key[i]] = get_rand_object();
+      else if (rand == 1) data[key[i]] = get_rand_int(0, 1e9 + 7);
+      else if (rand == 2) data[key[i]] = get_rand_float(1e9 + 7);
+      else if (rand == 3) data[key[i]] = get_rand_string(get_rand_int(16, 240));
+      else data[key[i]] = get_date();
+    }
+    datas[cur] = data;
+  }
 
-	var collection = db.collection('test');
-	collection.insert(datas, function(err, result) {
-		if (err) {
-			console.log('Error: ' + err);
+  var collection = db.collection('test');
+  collection.insert(datas, function(err, result) {
+    if (err) {
+      console.log('Error: ' + err);
       db.close();
-			return;
-		}
-		if (!isIndex) {
-			console.log("数据插入完成！");
-			db.close();
-		}
-	});
-	if (isIndex) {
-		console.log("建立索引...");
-		collection.ensureIndex(key[0], function(err, result) {
-			if (err) {
-				console.log('Error: ' + err);
-				return;
-			}
-			console.log("索引建立完毕！");
-			db.close();
-		});
-	}
+      return;
+    }
+    if (!isIndex) {
+      console.log("数据插入完成！");
+      db.close();
+    }
+  });
+  if (isIndex) {
+    console.log("建立索引...");
+    collection.ensureIndex(key[0], function(err, result) {
+      if (err) {
+        console.log('Error: ' + err);
+        return;
+      }
+      console.log("索引建立完毕！");
+      db.close();
+    });
+  }
 }
 var Query = function(db, callback, key = null) {
-	var collection = db.collection('test');
-	collection.find(key).toArray(function(err, result) {
-		if (err) {
-			console.log("Error: " + err);
-			return;
-		}
-		callback(result);
-	});
+  var collection = db.collection('test');
+  collection.find(key).toArray(function(err, result) {
+    if (err) {
+      console.log("Error: " + err);
+      return;
+    }
+    callback(result);
+  });
 }
 var Delete = function(db, key = null) {
-	var collection = db.collection('test');
-	Query(db, function(result) {
-		for (var i = 0; i < result.length; ++i) {
-			collection.remove(result[i], function(err, res) {
-				if (err) {
-					console.log("Error: " + err);
-					return;
-				}
-			});
-		}
+  var collection = db.collection('test');
+  Query(db, function(result) {
+    for (var i = 0; i < result.length; ++i) {
+      collection.remove(result[i], function(err, res) {
+        if (err) {
+          console.log("Error: " + err);
+          return;
+        }
+      });
+    }
     console.log("删除完成！");
-		db.close();
-	}, key);
+    db.close();
+  }, key);
 }
 var Update = function(db, key = null, value = null) {
-	var collection = db.collection('test');
-	Query(db, function(result) {
+  var collection = db.collection('test');
+  Query(db, function(result) {
     if (value == null) value = [];
     for (var i = 0; i < result.length; ++i) {
       var tmp = {};
@@ -182,25 +182,25 @@ var Update = function(db, key = null, value = null) {
 }
 
 var main = function(type, index = false, key = null) {
-	mongo_client.connect(db_url, function(err, db) {
-		if (!err) console.log("连接成功");
-		if (type == "insert") {
-			Insert(db, index);
-		} else if (type == "query") {
-			Query(db, function(result) {
-				for (var i = 0; i < result.length; ++i) {
+  mongo_client.connect(db_url, function(err, db) {
+    if (!err) console.log("连接成功");
+    if (type == "insert") {
+      Insert(db, index);
+    } else if (type == "query") {
+      Query(db, function(result) {
+        for (var i = 0; i < result.length; ++i) {
           console.log(result[i]);
         }
-				db.close();
-			}, key);
-		} else if (type == "delete") {
-			Delete(db, key);
-		} else if (type == "update") {
+        db.close();
+      }, key);
+    } else if (type == "delete") {
+      Delete(db, key);
+    } else if (type == "update") {
       Update(db);
     } else {
       console.log("function argument wrong");
     }
-	});
+  });
 };
 
 var argc = 0;
