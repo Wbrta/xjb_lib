3d5e83f56c8ae969e6f1b3cfbb53d24ec66086db
Wbrta <ghlovezxcvbnm@vip.qq.com>
2018-07-15 23:20:56 +0800

    add shell/connect.sh and modify README.md by add some information about this repository.

--- README.md
+++ README.md
@@ -5,3 +5,27 @@ This repository is my own library.
 Here will store some code which I write when I learn or work.
 And maybe this code can be use in some environment.
 And this repository is only something i play.
+
+## Completed File
+I have written some code which probabely will be updated a few days later.
+Here are the lists:
+| File Name | Introduction | Last Update |
+| :-------: | :----------: | :---------: |
+| cpp/skip_list.h | a simple version of skip list and may can be used in work | 2017.10.26 |
+| nodejs/mongo.js | a javascript file in order to operate mongodb | 2017.10.26 |
+| py/dict.py | a spider in order to crawl dict.youdao.com | 2018.1.22 |
+| py/ctrip.py | a spider to crawl ctrip data | 2018.3.20 |
+| py/lvmama.py | a spider to crawl lvmama data | 2018.3.20 |
+| py/qunar.py | a spider to crawl qunar data | 2018.3.20 |
+| py/tuniu.py | a spider to crawl tuniu data | 2018.3.20 |
+| py/peppa_pig.py | draw a peppa pig but only have head | 2018.5.8 |
+| shell/install.sh | install some software or lib quickly in linux | 2017.11.25 |
+| shell/connect.sh | connect host easily | 2018.7.15 |
+
+## Others
+The `test` folder is a test folder.
+Any file I have not listed above was not completed.
+I will complete them in some days.
+By the way, my English is very poor. So if you find any mistake in this file, please forgive me.
+Or you can tell me by mail to <wuxueyang96@gmail.com>.
+Thank you very much.
---
+++ shell/connect.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+FILE_NOT_EXIST=-1
+PARAMETER_LOSS=-2
+
+HOSTS_FILE=~/.CONNECT_HOST
+
+if [ ! -f $HOSTS_FILE ]; then
+  echo "Plese use $HOSTS_FILE to specify the hosts group and username and ip."
+  exit $FILE_NOT_EXIST
+fi
+
+declare -i i=0
+declare -i row_num=0
+declare -i col_num=0
+
+while read line
+do
+  if [[ $line =~ \[(.*?)\] ]]; then
+    group=$(echo $line | cut -d '[' -f2 | cut -d ']' -f1)
+    groups[$i]=$group
+    i=$i+1
+  else
+    if [[ $line =~ ^$ ]]; then
+      row_num=$row_num+1
+      col_num=0
+    else
+      arr_line=($line)
+      username=${arr_line[0]}
+      ip=${arr_line[1]}
+      eval host_ip_group_$row_num[$col_num]=$ip
+      eval host_username_group_$row_num[$col_num]=$username
+      col_num=$col_num+1
+    fi
+  fi
+done < $HOSTS_FILE
+
+# echo ${groups[@]}
+# for ((i=0;i<row_num;i++))
+# do
+#   eval echo \${host_ip_group_$i[@]}
+#   eval echo \${host_username_group_$i[@]}
+# done
+
+group_info="Please choise a group:"
+host_info="Please choise a host:"
+
+for ((i=0;i<row_num;i++))
+do
+  group_info=$group_info"\n\t"$i." "${groups[$i]}
+done
+echo -e $group_info
+read -p "your choice: " group
+
+eval host_number=\${#host_ip_group_$group[@]}
+eval validation=\${#host_username_group_$group[@]}
+if [ $host_number -ne $validation ]; then
+  echo "You must specify a username or ip for every host in ~/.CONNECT_HOST"
+  exit $PARAMETER_LOSS
+fi
+for ((i=0;i<$host_number;i++))
+do
+  eval ip=\${host_ip_group_$group[$i]}
+  eval username=\${host_username_group_$group[$i]}
+  host_info=$host_info"\n\t"$i." "$username"\t"$ip
+done
+echo -e $host_info
+read -p "your choice: " host
+
+eval ip=\${host_ip_group_$group[$host]}
+eval username=\${host_username_group_$group[$host]}
+ssh $username@$ip
