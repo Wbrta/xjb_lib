f7f50da2fabdec52b89a9aa8ed3083f41f33fb85
wuxueyang2 <wuxueyang1@jd.com>
2018-10-15 11:26:56 +0800

    make dict can query more times

--- py/dict.py
+++ py/dict.py
@@ -1,19 +1,26 @@
 #-*- coding=utf8 -*-
+import os
 import urllib
 from bs4 import BeautifulSoup
 
-#word = "leave"
-word = input("Please input word: ")
+while True:
+  #word = "leave"
+  word = input("Please input word: ")
+  if word == 'Q':
+      break
 
-url = "http://dict.youdao.com/w/" + word + "/"
-html = urllib.request.urlopen(url).read()
+  url = "http://dict.youdao.com/w/" + word + "/"
+  html = urllib.request.urlopen(url).read()
 
-soup = BeautifulSoup(html, "lxml")
-results = soup.find_all("div", class_="trans-container")[0].find_all("li")
+  soup = BeautifulSoup(html, "lxml")
+  results = soup.find_all("div", class_="trans-container")[0].find_all("li")
 
-print ("======================================")
+  print ("======================================")
 
-for result in results:
-  res = str(result)
-  res = res.replace("<li>", "").replace("</li>", "")
-  print (res)
+  for result in results:
+    res = str(result)
+    res = res.replace("<li>", "").replace("</li>", "")
+    print (res)
+  
+  tmp = input("Press enter to continue...")
+  os.system("cls")
